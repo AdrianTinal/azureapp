e9a6174f74f0cdc2b4e027392bbc212950a0a683
AdrianTinal <adrian.tinal@gmail.com>
2026-03-02 17:45:07 -0600

    CICD azure

---
+++ fronting/src/app.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const message = document.getElementById('message');
+    message.textContent = 'Esta aplicación está funcionando y lista para desplegarse en Azure.';
+    console.log('App inicializada');
+});
---
+++ terraform.tfvars
@@ -0,0 +1,2 @@
+subscription_id = "442b45a3-01be-4f9c-88b2-bf2a51182c5b"
+location = "East US"
