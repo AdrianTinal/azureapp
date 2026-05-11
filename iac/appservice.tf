d6f055f6f8eccc581caeb9a1e67b59b572660ed6
AdrianTinal <adrian.tinal@gmail.com>
2026-05-11 15:24:16 -0600

    fix: use fixed app name for predictable CI/CD configuration

--- iac/appservice.tf
+++ iac/appservice.tf
@@ -1,9 +1,3 @@
-resource "random_string" "suffix" {
-  length  = 6
-  special = false
-  upper   = false
-}
-
 resource "azurerm_service_plan" "asp" {
   name                = "asp-${var.project_name}"
   resource_group_name = azurerm_resource_group.rg.name
@@ -13,7 +7,7 @@ resource "azurerm_service_plan" "asp" {
 }
 
 resource "azurerm_linux_web_app" "app" {
-  name                = "app-${var.project_name}-${random_string.suffix.result}"
+  name                = "app-${var.project_name}-prod"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   service_plan_id     = azurerm_service_plan.asp.id
